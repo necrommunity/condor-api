b0f2bc34a8b0958ae8a01d5b8ed7fd651a48a0c1
MacND <johnny187896@live.co.uk>
2018-02-16 14:14:47 +0000

    Enable setting schema using

--- event.php
+++ event.php
@@ -1,9 +1,30 @@
 <?php
+
 header('Content-Type: application/json');
 
-if (!empty($_GET["schema"])) {
-  $schema = $_GET["schema"];
+if (!empty($_REQUEST['req'])) {
+    $schema_req = $_REQUEST['req'];
+
+   $schema_split = explode('/', $schema_req);
+
+    if (count($schema_split) == 1) {
+        $schema = strtolower($schema_split[0]);
+    }
+}
+
+if (!empty($_REQUEST["schema"])) {
+  $schema = $_REQUEST["schema"];
+
+} elseif (!empty($_REQUEST['req'])) {
+  $schema_req = $_REQUEST['req'];
+
+  $schema_split = explode('/', $schema_req);
+
+  if (count($schema_split) == 1) {
+    $schema = strtolower($schema_split[0]);
+   }
 } else {
+
   $configFile = '/home/bot/necrobot/data/condorbot_config';
   $configData = file_get_contents($configFile);
   $rows = explode("\n", $configData);
@@ -37,10 +58,9 @@ if (in_array($schema,$databases)){
   $stmt = $pdo->prepare("SELECT m.match_id, DATE_FORMAT(suggested_time, '%Y-%m-%dT%TZ') 'suggested_time', a.rtmp_name 'racer_1', d.racer_1_wins 'racer_1_wins', b.rtmp_name 'racer_2', d.racer_2_wins 'racer_2_wins', c.twitch_name 'commentary', DATE_FORMAT(finish_time, '%Y-%m-%dT%TZ') 'finish_time' FROM matches m INNER JOIN necrobot.users a ON a.user_id=racer_1_id INNER JOIN necrobot.users b ON b.user_id=racer_2_id LEFT JOIN necrobot.users c ON c.user_id=cawmentator_id LEFT JOIN match_info d ON d.match_id=m.match_id WHERE r1_confirmed = 1 AND r2_confirmed = 1 ORDER BY suggested_time ASC;");
   $stmt->execute();
   $matches = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
   $eventData['matches'] = $matches;
 
-
+  //$matches variable will be used to stored results of individual race details in a nested array
 
   $stmt = $pdo->prepare("SELECT league_name, a.character 'character', is_best_of, number_of_races, deadline FROM league_info INNER JOIN necrobot.race_types a ON a.type_id = race_type;");
   $stmt->execute();
@@ -59,7 +79,7 @@ if (in_array($schema,$databases)){
 } else {
   echo "<pre>Input schema is not available.</pre>";
 }
+
 echo json_encode($eventData,JSON_PRETTY_PRINT);
-//echo '<pre>',json_encode($league_info,JSON_PRETTY_PRINT),'</pre>';
-//echo $eventData['event_name']; 
+
 ?>
