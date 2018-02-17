d400a195b629525a30a7cd7ad26a0c2562988b93
MacND <johnny187896@live.co.uk>
2018-02-17 14:04:16 +0000

    /event now gets its list of events from the /events endpoint instead of a SQL query

--- event.php
+++ event.php
@@ -2,6 +2,9 @@
 
 header('Content-Type: application/json');
 
+$events = file_get_contents('https://condor.live/api/events');
+$events = json_decode($events, true);
+
 if (!empty($_REQUEST['req'])) {
     $schema_req = $_REQUEST['req'];
 
@@ -48,12 +51,7 @@ $eventData = array(
 	matches => array()
 );
 
-$pdo = new PDO("mysql:host=localhost",'necrobot-read','necrobot-read');
-$stmt = $pdo->prepare("SELECT SCHEMA_NAME FROM information_schema.SCHEMATA WHERE SCHEMA_NAME NOT IN ('information_schema', 'performance_schema', 'necrobot', 'mysql');");
-$stmt->execute();
-$databases = $stmt->fetchAll(PDO::FETCH_COLUMN);
-
-if (in_array($schema,$databases)){
+if (in_array($schema,$events['events'])){
   $pdo = new PDO("mysql:host=localhost;dbname=$schema",'necrobot-read','necrobot-read');
   $stmt = $pdo->prepare("SELECT m.match_id, DATE_FORMAT(suggested_time, '%Y-%m-%dT%TZ') 'suggested_time', a.rtmp_name 'racer_1', d.racer_1_wins 'racer_1_wins', b.rtmp_name 'racer_2', d.racer_2_wins 'racer_2_wins', c.twitch_name 'commentary', DATE_FORMAT(finish_time, '%Y-%m-%dT%TZ') 'finish_time' FROM matches m INNER JOIN necrobot.users a ON a.user_id=racer_1_id INNER JOIN necrobot.users b ON b.user_id=racer_2_id LEFT JOIN necrobot.users c ON c.user_id=cawmentator_id LEFT JOIN match_info d ON d.match_id=m.match_id WHERE r1_confirmed = 1 AND r2_confirmed = 1 ORDER BY suggested_time ASC;");
   $stmt->execute();
